01c751519a3eb833a795c27ce909464598c24475
devjahid1 <developerjahid1@gmail.com>
2023-05-31 03:29:48 +0600

    first commit

---
+++ class6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class class6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                //Question 1 //
+        System.out.println("Question No. 1");
+
+        System.out.println("Enter some number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Number 2 : ");
+        int b = sc.nextInt();
+        System.out.println("Enter Number 3 : ");
+        int c = sc.nextInt();
+        int sum = a + b + c;
+        System.out.println("Total Number Is = "+sum);
+
+
+
+        
+    }
+}
