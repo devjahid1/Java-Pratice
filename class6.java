0b4cf0ca520f6a1e1c23cfd60c96e9a1734b904b
devjahid1 <developerjahid1@gmail.com>
2023-05-31 04:06:38 +0600

    first commit

--- class6.java
+++ class6.java
@@ -14,6 +14,21 @@ public class class6 {
         int sum = a + b + c;
         System.out.println("Total Number Is = "+sum);
 
+                //Question 2 //
+
+    System.out.println("Enter Subject 1 = ");
+    float d = sc.nextFloat();
+    System.out.println("Enter Subject 2 = ");
+    float e = sc.nextFloat();
+    System.out.println("Enter Subject 3 = ");
+    float f = sc.nextFloat();
+    float sum2 = (d + e + f)/3;
+    System.out.println("Total Gpa is = "+sum);
+
+            //Question 3//
+    System.out.print("Enter Your Name : ");
+    String str = sc.next();
+    System.out.println("Hello "+str+" Good Morning");
 
 
         
