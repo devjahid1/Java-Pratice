f6297c4f6ccb3dcaf2a4c8dbccb4300a7ee028d0
MD. Jahid Miah <95177076+devjahid1@users.noreply.github.com>
2023-06-02 04:54:29 +0600

    Add files via upload

---
+++ class7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class class7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //System.out.print("Enter Kilometre = ");
+
+        //Question 4//
+
+        //double Kilometre = sc.nextFloat();
+        //double mile = Kilometre * 0.621371;
+        //System.out.println("Your Enterd Kilometre = "+Kilometre);
+        //System.out.println("Mile is = "+mile);
+
+        //Question5//
+
+        System.out.print("Enter Integer = ");
+        System.out.println(sc.hasNextInt());
+
+        
+    }
+}
