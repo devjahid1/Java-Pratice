8b1a18461d9136d6dedf8aafeb789a5a15358adb
devjahid1 <developerjahid1@gmail.com>
2023-06-02 04:51:42 +0600

    first commit

--- class7.java
+++ class7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class class7p {
+public class class7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //System.out.print("Enter Kilometre = ");
