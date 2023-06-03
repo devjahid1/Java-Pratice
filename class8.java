4c7070bf1089e7a251c67b90cad8ed401f66ed83
devjahid1 <developerjahid1@gmail.com>
2023-06-04 03:23:11 +0600

    first commit

---
+++ class8.java
@@ -0,0 +1,12 @@
+//operators
+public class class8 {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 10 + a;
+        System.out.println(b);
+        
+
+    }
+}
