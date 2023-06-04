528303324259a488cb5a78d2341defaff67dbe9b
MD. Jahid Miah <95177076+devjahid1@users.noreply.github.com>
2023-06-04 21:02:47 +0600

    Add files via upload

---
+++ pratice2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class pratice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int sum = A + B;
+        System.out.println("SOMA = " +sum);
+    }
+}
