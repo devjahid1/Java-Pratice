d25b278ba988f167079484b6e92231f96cd8aa5c
MD. Jahid Miah <95177076+devjahid1@users.noreply.github.com>
2023-06-04 20:11:49 +0600

    Add files via upload

---
+++ class8.java
@@ -0,0 +1,27 @@
+//operators
+public class class8 {
+
+    public static void main(String[] args) {
+
+        //Arithmatic Operator
+        int a = 9;
+        int b = 10 + a;
+        System.out.println(b);
+
+        //Assignment Operator
+        int c = 10;
+        c += 10;
+        System.out.println(c);
+
+        //Comparison Operator
+        System.out.println(6==6);
+        System.out.println(6>=10);
+        System.out.println(6 <= 10);
+
+        //Logical Operator
+        System.out.println(6<5 && 6<10);
+        System.out.println(6<10 || 6>10 );
+        
+        
+    }
+}
