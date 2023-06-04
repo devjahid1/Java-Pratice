76c01bd488ffa4c525a4db66a81ab57d08f56c1c
MD. Jahid Miah <95177076+devjahid1@users.noreply.github.com>
2023-06-04 20:49:41 +0600

    Add files via upload

---
+++ pratice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class pratice {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        double R = sc.nextDouble();
+        double A = 3.14159 * R*R;
+        System.out.printf("A=%.4f\n", A);
+    }
+}
