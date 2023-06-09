916d6dd01148bb1c70a7b046c127437757633f71
devjahid1 <developerjahid1@gmail.com>
2023-06-10 03:01:46 +0600

    first commit

---
+++ class10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class class10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i--);
+        System.out.println(i);
+        System.out.println(--i);
+        System.out.println(i);
+    }
+}
