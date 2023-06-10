7ea04eb41ca2bc9965bdeaeb1dd06038500d31ad
devjahid1 <developerjahid1@gmail.com>
2023-06-10 20:33:08 +0600

    first commit

--- class10.java
+++ class10.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-public class class10 {
-    public static void main(String[] args) {
+public class class10{
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number = ");
         int i = sc.nextInt();
         System.out.println(i--);
         System.out.println(i);
