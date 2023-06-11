053880eb2ed1e53a0a809e833c4a7ed9dbefa200
devjahid1 <developerjahid1@gmail.com>
2023-06-12 01:06:09 +0600

    first class

---
+++ class11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class class11 {
+    public static void main(String[] args) {
+        char Grade = 'B';
+        Grade =(char)(Grade+8);
+        System.out.println(Grade);
+
+        Grade = (char)(Grade-8);
+        System.out.println(Grade);
+    }
+}
