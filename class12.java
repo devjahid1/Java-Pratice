e8ea6956c4468ae484ea7bf0a05573234cea4660
devjahid1 <developerjahid1@gmail.com>
2023-06-12 01:37:05 +0600

    first commit

---
+++ class12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class class12 {
+    public static void main(String[] args) {
+      //  String name = new String("JAHID Miah");
+      //String name = "JAHID";
+       // System.out.print("The Name is : ");
+       // System.out.println(name);
+
+     //  int a = 10;
+     //  float b = 20;
+     //  System.out.printf("The value of a is %d and the value of b is %f\n",a,b);
+     //  System.out.format("The value of a is %d and the value of b is %f",a,b);
+
+     Scanner sc = new Scanner(System.in);
+     //String st = sc.next();
+     String st = sc.nextLine();
+     System.out.println(st);
+
+    }
+}
