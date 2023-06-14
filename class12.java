90403d99f3cb8702170e884f72dc095939944f8a
devjahid1 <developerjahid1@gmail.com>
2023-06-15 04:22:06 +0600

    first commit

--- class12.java
+++ class12.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-
 public class class12 {
     public static void main(String[] args) {
-      //  String name = new String("JAHID Miah");
-      //String name = "JAHID";
-       // System.out.print("The Name is : ");
-       // System.out.println(name);
+      //String str = new String("Hello Jahid");
+      //System.out.println(str);
+      //String name = "Hello Jahid";
+      //System.out.println(name);
+      Scanner sc = new Scanner(System.in);
+      String str = sc.nextLine();
+      System.out.println(str);
 
-     //  int a = 10;
-     //  float b = 20;
-     //  System.out.printf("The value of a is %d and the value of b is %f\n",a,b);
-     //  System.out.format("The value of a is %d and the value of b is %f",a,b);
-
-     Scanner sc = new Scanner(System.in);
-     //String st = sc.next();
-     String st = sc.nextLine();
-     System.out.println(st);
 
     }
 }
---
+++ class13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class class13 {
+    public static void main(String[] args) {
+        String a = "Harry";
+       // Scanner sc = new Scanner(System.in);
+       // String b = sc.nextLine();
+       // String nontrimmedString = nontrimmedString.trim(a);
+       // System.out.println(nontrimmedString);
+       // System.out.println(a.substring(1, 9));
+       // System.out.println(a.substring(5));
+       // System.out.println(a.replace('r', 'p'));
+       // System.out.println(a.startsWith("art"));
+       // System.out.println(a.startsWith("Har"));
+       // System.out.println(a.endsWith("ry"));
+       // System.out.println(a.charAt(4));
+
+    }
+}
