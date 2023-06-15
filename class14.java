6c9211fcb9f82167ef6110cd2a3090ea698a564d
devjahid1 <developerjahid1@gmail.com>
2023-06-15 16:50:15 +0600

    first commit

--- class14.java
+++ class14.java
@@ -1,27 +1,27 @@
 public class class14 {
     public static void main(String[] args) {
-// Problem 1
-        //String name = "Jack Parker";
-        //name = name.toLowerCase();
-        //System.out.println(name);
 
-        // Problem 2
-        //String text = "To My     Friend";
-        //text = text.replace(" ", "_");
-        //System.out.println(text);
+        // Problem Set-1
+        String name = "JAHID";
+        name = name.toLowerCase();
+        System.out.println(name);
+        
+        // Problem set-2
+        String space = "Hello     Jahid";
+        space = space.replace(" ", "_");
+        System.out.println(space);
 
-        // Problem 3
-        String letter = "Dear <|name|>, Thanks a lot!";
-        letter = letter.replace("<|name|>", "Sachin");
+        // Problem set-3
+        String name2 = "Hello I am <|name|> . And I am a Progeammer";
+        System.out.println(name2.replace("<|name|>", "Jahid")); 
+
+        // Problem set-4
+        String doubleSpace = "Here is  double space and here is truple spaces";
+        System.out.println(doubleSpace.indexOf("  "));
+        System.out.println(doubleSpace.indexOf("   "));
+
+        // Problem set-5
+        String letter = "Hello, \n I am jahid";
         System.out.println(letter);
-
-        // Problem 4
-        String myString = "This string contains double and  triple spaces";
-        System.out.println(myString.indexOf("  "));
-        System.out.println(myString.indexOf("   "));
-
-        // Problem 5
-        String myLetter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";
-        System.out.println(myLetter);
     }
 }
